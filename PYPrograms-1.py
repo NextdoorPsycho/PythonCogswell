f784ccddac5c60d532464512bdc5c7bbc4ce9c0d
Brian <mypsn1550@gmail.com>
2019-08-08 15:29:24 -0700

    htf

--- PYPrograms-1.py
+++ PYPrograms-1.py
@@ -492,12 +492,18 @@ Made by [Brian Fopiano] at Cogswell Polytech, for
                 except ValueError:
                     print("This is NOT a value Try again!")
         main()
+<<<<<<< Updated upstream
 #MODULE EIGHT HERE
     def module8():
+=======
+#MODULE 305 HERE
+    def module7():
+>>>>>>> Stashed changes
         def main():
             active = True
 
             while active:
+<<<<<<< Updated upstream
                 print( "This program is to showcase the assignments done!" )
                 print("MODULE 6 EDITION! [PASSWORDS] Press 1 to start, or back to Everything [0]: ")
                 time.sleep(0.1)
@@ -526,6 +532,99 @@ Made by [Brian Fopiano] at Cogswell Polytech, for
                                 pass
                         _5000()
                         #END OF MODULE
+=======
+                print( "." )
+                print("MODULE: ")
+                time.sleep(0.1)
+                try:
+                    PNum = int(input( "Start program? [1] Go back [0] " ))      
+                    if PNum == 1:
+                        def stuff():
+                            win = GraphWin('Smiley Faces', 400, 400) # give title and dimensions
+                            win.setBackground('cyan')
+                            win.setCoords(0, 0, 400, 400)
+                            
+                            def drawFace(center, size,window):
+                            
+                                head = Circle(center,size*20)
+                                head.setFill("green")
+                                head.draw(win)
+                            
+                                mouth = Circle(center, size*13)
+                                mouth.setFill("red")
+                                mouth.setOutline("red")
+                                mouth.draw(win)
+                                smile = Circle(center, size*14)
+                                smile.move(0,size*4)
+                                smile.setFill("green")
+                                smile.setOutline("green")
+                                smile.draw(win)
+                            
+                                eyebrow = Circle(center,size*4)
+                                eyebrow.move(-size*8,size*10)
+                                eyebrow.setFill('black')
+                                eyebrow.draw(win)
+                                eyebrow2 = eyebrow.clone()
+                                eyebrow2.draw(win)
+                                eyebrow2.move(size*16, 0)
+                                eyecircle = Circle(center,size*4)
+                                eyecircle.move(-size*8,size*9)
+                                eyecircle.setFill('green')
+                                eyecircle.setOutline('green')
+                                eyecircle.draw(win)
+                                eyecircle2 = eyecircle.clone()
+                                eyecircle2.draw(win)
+                                eyecircle2.move(size*16, 0)
+                            
+                                eyelid = Circle(center,size*3)
+                                eyelid.move(-size*8,size*8)
+                                eyelid.setFill('brown')
+                                eyelid.draw(win)
+                                eyelid2 = eyelid.clone()
+                                eyelid2.draw(win)
+                                eyelid2.move(size*16, 0)
+                            
+                                eye = Circle(center, size*3)
+                                eye.move(-size*8,size*6)
+                                eye.setFill('orange')
+                                eye.draw(win)
+                                eye2 = eye.clone()
+                                eye2.draw(win)
+                                eye2.move(size*16, 0)
+                            
+                                pupil = Circle(center,size)
+                                pupil.move(-size*9,size*7)
+                                pupil.setFill('blue')
+                                pupil.draw(win)
+                                pupil2 = pupil.clone()
+                                pupil2.draw(win)
+                                pupil2.move(size*16,0)
+                            
+                                nose = Circle(center,size*3)
+                                nose.move(0,-size*2)
+                                nose.setOutline('yellow')
+                                nose.setFill('yellow')
+                                nose.draw(win)
+                            
+                            def main():
+                            
+                                i = 0
+                                for i in range (1,5):
+                                    center = Point(350,490-i*110)#top to bottom, increasing radius
+                                    Face = drawFace(center,i*.8,win)
+                                    center = Point(-50+i*85,-50+i*90)#bottom left to top right, decreasing radius
+                                    Face = drawFace(center,3-(i*.5),win)
+                                    center = Point(340-i*75,-65+i*100)#bottom right to top left, increasing radius
+                                    Face = drawFace(center,i,win)
+                            
+                                message = Text(Point(200, 380), 'Click anywhere to quit.')
+                                message.setFill('blue')
+                                message.draw(win)
+                                win.getMouse()
+                                win.close()
+                            
+                            main()
+>>>>>>> Stashed changes
                     elif PNum == 0:
                         print("Goodbye!")
                         active = False
@@ -535,6 +634,10 @@ Made by [Brian Fopiano] at Cogswell Polytech, for
                 except ValueError:
                     print("This is NOT a value Try again!")
         main()
+<<<<<<< Updated upstream
+=======
+#MODULE IDENTIFICATION HERE
+>>>>>>> Stashed changes
 #MODULE IDENTIFICATION HERE
     try:
         
@@ -578,3 +681,89 @@ Made by [Brian Fopiano] at Cogswell Polytech, for
 #Run it! 
 Everything()
 
+from graphics import *
+ 
+win = GraphWin('Smiley Faces', 400, 400) # give title and dimensions
+win.setBackground('cyan')
+win.setCoords(0, 0, 400, 400)
+ 
+def drawFace(center, size,window):
+ 
+    head = Circle(center,size*20)
+    head.setFill("green")
+    head.draw(win)
+ 
+    mouth = Circle(center, size*13)
+    mouth.setFill("red")
+    mouth.setOutline("red")
+    mouth.draw(win)
+    smile = Circle(center, size*14)
+    smile.move(0,size*4)
+    smile.setFill("green")
+    smile.setOutline("green")
+    smile.draw(win)
+ 
+    eyebrow = Circle(center,size*4)
+    eyebrow.move(-size*8,size*10)
+    eyebrow.setFill('black')
+    eyebrow.draw(win)
+    eyebrow2 = eyebrow.clone()
+    eyebrow2.draw(win)
+    eyebrow2.move(size*16, 0)
+    eyecircle = Circle(center,size*4)
+    eyecircle.move(-size*8,size*9)
+    eyecircle.setFill('green')
+    eyecircle.setOutline('green')
+    eyecircle.draw(win)
+    eyecircle2 = eyecircle.clone()
+    eyecircle2.draw(win)
+    eyecircle2.move(size*16, 0)
+ 
+    eyelid = Circle(center,size*3)
+    eyelid.move(-size*8,size*8)
+    eyelid.setFill('brown')
+    eyelid.draw(win)
+    eyelid2 = eyelid.clone()
+    eyelid2.draw(win)
+    eyelid2.move(size*16, 0)
+ 
+    eye = Circle(center, size*3)
+    eye.move(-size*8,size*6)
+    eye.setFill('orange')
+    eye.draw(win)
+    eye2 = eye.clone()
+    eye2.draw(win)
+    eye2.move(size*16, 0)
+ 
+    pupil = Circle(center,size)
+    pupil.move(-size*9,size*7)
+    pupil.setFill('blue')
+    pupil.draw(win)
+    pupil2 = pupil.clone()
+    pupil2.draw(win)
+    pupil2.move(size*16,0)
+ 
+    nose = Circle(center,size*3)
+    nose.move(0,-size*2)
+    nose.setOutline('yellow')
+    nose.setFill('yellow')
+    nose.draw(win)
+ 
+def main():
+ 
+    i = 0
+    for i in range (1,5):
+        center = Point(350,490-i*110)#top to bottom, increasing radius
+        Face = drawFace(center,i*.8,win)
+        center = Point(-50+i*85,-50+i*90)#bottom left to top right, decreasing radius
+        Face = drawFace(center,3-(i*.5),win)
+        center = Point(340-i*75,-65+i*100)#bottom right to top left, increasing radius
+        Face = drawFace(center,i,win)
+ 
+    message = Text(Point(200, 380), 'Click anywhere to quit.')
+    message.setFill('blue')
+    message.draw(win)
+    win.getMouse()
+    win.close()
+ 
+main()
